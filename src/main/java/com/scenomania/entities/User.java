6d4e66c0950272ef5138786be323ef210b990813
Eugene Panaitov <eugene.panaitov@gmail.com>
2010-06-11 01:18:55 +0400

    validation tomcat test

--- src/main/java/com/scenomania/entities/User.java
+++ src/main/java/com/scenomania/entities/User.java
@@ -4,10 +4,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name="users")
@@ -15,6 +13,7 @@ public class User extends EntityBase {
 
 	@NotNull
 	@Column
+	@Length(min=1,max=250)
     private String nickname;
 
 	//@NotNull
