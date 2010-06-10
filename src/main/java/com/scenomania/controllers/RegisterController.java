a7d8118732573a17f80cc7ec14ffcf0e2bddc23e
Eugene Panaitov <eugene.panaitov@gmail.com>
2010-06-11 00:17:38 +0400

    validation tomcat test

--- src/main/java/com/scenomania/controllers/RegisterController.java
+++ src/main/java/com/scenomania/controllers/RegisterController.java
@@ -2,16 +2,13 @@ package com.scenomania.controllers;
 
 import com.scenomania.entities.User;
 import com.scenomania.services.UserService;
-//import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import javax.validation.Validation;
-//import javax.validation.Validator;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
--- src/main/java/com/scenomania/entities/User.java
+++ src/main/java/com/scenomania/entities/User.java
@@ -7,7 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="users")
