d5b3fefebd8eedb33084752dbcdecf2b90c96b39
Eugene Panaitov <eugene.panaitov@gmail.com>
2010-11-21 01:53:37 +0300

    expaning sitemap and refreshing index

---
+++ src/main/java/com/scenomania/utils/CityOnMap.java
@@ -0,0 +1,16 @@
+package com.scenomania.utils;
+
+/**
+ *
+ * @author eugene
+ */
+public class CityOnMap {
+	public Integer id;
+	public Double lat;
+	public Double lng;
+	public Integer pop;
+	public String name;
+	public String slug;
+	public String countrySlug;
+	public String areaCode;
+}
